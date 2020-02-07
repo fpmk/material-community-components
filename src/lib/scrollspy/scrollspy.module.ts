9d2214104b3063f5fcdd197e7602a10f5e1c3357
fpmk1122@gmail.com <2fq3eatb>
2020-02-07 11:00:15 +0700

    chore: updated to ng9

--- src/lib/scrollspy/scrollspy.module.ts
+++ src/lib/scrollspy/scrollspy.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 
 import { MccScrollspyService } from './scrollspy.service';
 import { MccScrollspyGroupDirective, MccScrollspyItemDirective } from './scrollspy.directives';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  imports: [CommonModule, ScrollDispatchModule],
-  providers: [MccScrollspyService, { provide: 'Window', useValue: window }],
-  declarations: [MccScrollspyGroupDirective, MccScrollspyItemDirective],
-  exports: [MccScrollspyGroupDirective, MccScrollspyItemDirective],
+  imports: [ CommonModule, ScrollingModule ],
+  providers: [ MccScrollspyService, { provide: 'Window', useValue: window } ],
+  declarations: [ MccScrollspyGroupDirective, MccScrollspyItemDirective ],
+  exports: [ MccScrollspyGroupDirective, MccScrollspyItemDirective ],
 })
-export class MccScrollspyModule {}
+export class MccScrollspyModule {
+}
